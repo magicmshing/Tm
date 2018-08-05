0dbd8a6f80bc2cd818939b1d560610521c1627b3
mshing <mshing@yeah.net>
2018-08-05 22:46:21 +0800

    first commit my wechat miniprogram

---
+++ pages/index/index.js
@@ -0,0 +1,86 @@
+//index.js
+
+// 映射动态天气对应的中文天气
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+// 设置动态导航栏颜色
+const weatherColorMap = {
+  'sunny': 'c0dcff',
+  'cloudy': 'bcdfec',
+  'overcast': 'c0dcff',
+  'lightrain': 'd6d4fb',
+  'heavyrain': 'bcbae5',
+  'snow': 'e2eef2'
+}
+
+
+
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBackground: '',  //     动态获取与天气相对应的背景图片
+    hourlyWeather: [],
+  },
+  enablePullDownRefresh() {
+    this.getnow(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad() {
+    this.getnow()
+  },
+  getnow(callback) {
+    wx.request({
+      url: 'http://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+         console.log(res)
+         let result = res.data.result
+         this.setnow(result)
+         this.sethourlyWeather(result)         
+       },
+       complete:() => {
+         callback && callback()
+       }
+     })         
+  },
+  setnow(result){
+    let temp = result.now.temp
+    let weather = result.now.weather
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp + '°',
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png'
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather]
+    })
+  },
+  sethourlyWeather(result){
+    let forecast = result.forecast
+    let nowHour = new Date().getHours()
+    let hourlyWeather = []
+    for (let i = 0; i < 8; i += 1) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/images/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°',
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      hourlyWeather: hourlyWeather
+    })
+  },
+})

7e36c856fb3f2ad8d6cb2024ad28349280514c77
mshing <mshing@yeah.net>
2018-08-08 16:33:48 +0800

    course 6-7: Pass the parameters to the second page

--- pages/list/list.js
+++ pages/list/list.js
@@ -3,9 +3,13 @@ const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四',
 
 Page({
   data: {
-    weekWeather: [1, 2, 3, 4, 5, 6, 7]
+    weekWeather: [],
+    city:"广州市"
   },
-  onLoad() {
+  onLoad(options) {
+    this.setData({
+      city: options.city
+    })
     this.getWeekWeather()
   },
   onPullDownRefresh() {
@@ -18,7 +22,7 @@ Page({
       url: 'https://test-miniprogram.com/api/weather/future',
       data: {
         time: new Date().getTime(),
-        city: '广州市'
+        city: this.data.city
       },
       success: res => {
         let result = res.data.result

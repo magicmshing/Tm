f54623cf405e1742a1c9afd15677a748ecd470fb
mshing <mshing@yeah.net>
2018-08-11 22:44:25 +0800

    sourse 6-7:Processing position rejected

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,6 +20,14 @@ const weatherColorMap = {
 }
 const QQMapWX = require ('../../libs/qqmap-wx-jssdk.js')
 
+const UNPROMPTED = 0
+const UNAUTHORIZED = 1
+const AUTHORIZED = 2
+
+const UNPROMPTED_TIPS = "点击获取当前位置"
+const UNAUTHORIZED_TIPS = "点击开启位置权限"
+const AUTHORIZED_TIPS = ""
+
 Page({
   data: {
     nowTemp: '',
@@ -29,7 +37,8 @@ Page({
     todayTemp: "",
     todayDate: "",
     city: "广州市",
-    locationTipsText: "点击获取当前位置"
+    locationAuthType: UNPROMPTED,
+    locationTipsText: UNPROMPTED_TIPS
   },
   enablePullDownRefresh() {
     this.getnow(() => {
@@ -103,8 +112,15 @@ Page({
     })
   },
   onTapLocation() {
+      this.getLocation()
+  },
+  getLocation(){
     wx.getLocation({
       success: res => {
+        this.setData({
+          locationAuthType: AUTHORIZED,
+          locationTipsText: AUTHORIZED_TIPS
+        })
         //调用接口 
         this.qqmapsdk.reverseGeocoder({
           location: {
@@ -115,10 +131,15 @@ Page({
             let city = res.result.address_component.city
             this.setData({
               city: city,
-              locationTipsText: ""
             })
             this.getnow()
           }
+        })
+      },
+      fail: () => {
+        this.setData({
+          locationAuthType: UNAUTHORIZED,
+          locationTipsText: UNAUTHORIZED_TIPS
         })
       }
     })
